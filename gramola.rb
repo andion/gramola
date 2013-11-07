4b98c12b3ec6a508934f0eaa04917a153f81f6fb
Lucas Andión <andion@gmail.com>
2013-11-07 17:46:33 +0100

    Refactor gramola into module + Player class

--- gramola.rb
+++ gramola.rb
@@ -11,48 +11,58 @@
 #   Play specific groups/keywords: './gramola.rb 'Group name' Keyword 'Other group'
 # 
 #
-MUSIC_FOLDER='Music'
-MUSIC_EXTENSIONS='*{.mp3}'
 
-groups = ARGV
-search_string = File.join(Dir.home,MUSIC_FOLDER,'**',MUSIC_EXTENSIONS)
+module Gramola
+  MUSIC_FOLDER='Music'
+  MUSIC_EXTENSIONS='*{.mp3}'
 
-files = Dir[search_string]
+  class Player
 
-def filter_by_groups(files,groups)
-	if groups.any?
-		files.select{|f| f =~ Regexp.new("#{groups * '|'	}") }
-	else
-		files
-	end
-end
+    def initialize(argv)
+      files = Dir[File.join(Dir.home,MUSIC_FOLDER,'**',MUSIC_EXTENSIONS)]
+      @songs = filter_by_groups(files,argv)
+    end
 
-# Gets lenght in seconds from +afinfo+ command output
-def get_length(afinfo)
-	afinfo.lines[2].split[0].to_f
-end
+    def print_info
+      puts "Playing #{@songs.size} songs for about #{print_length(total_length(@files))}"
+    end
 
-# Returns total lenght of the +files+ in seconds
-def total_length(files)
-	files.inject(0){|acc,f| acc = acc + get_length(`afinfo -b "#{f}"`); acc}
-end
+    def play
+      @songs.shuffle.each do |s|
+        `afplay "#{s}"`
+    end
+      
+    private  
 
-def print_length(seconds)
-	m, s = seconds.divmod(60)
-	h, m = m.divmod(60)
-	d, h = h.divmod(24)
-	[d > 0 ? ("%d day" % d) : nil, h > 0 ? ("%d hours" % h) : nil,
-	  m ? ("%d minutes" % m) : ("%d seconds... Orly?")].compact * ' '	
-end
+    def filter_by_groups(files,groups)
+      if groups.any?
+        files.select{|f| f =~ Regexp.new("#{groups * '|'  }") }
+      else
+        files
+      end
+    end
 
-def print_info(files)
-	puts "Playing #{files.size} files for about #{print_length(total_length(files))}"
+    # Gets lenght in seconds from +afinfo+ command output
+    def get_length(afinfo)
+      afinfo.lines[2].split[0].to_f
+    end
+
+    # Returns total lenght of the +files+ in seconds
+    def total_length(files)
+      files.inject(0){|acc,f| acc = acc + get_length(`afinfo -b "#{f}"`); acc}
+    end
+
+    def print_length(seconds)
+      m, s = seconds.divmod(60)
+      h, m = m.divmod(60)
+      d, h = h.divmod(24)
+      [d > 0 ? ("%d day" % d) : nil, h > 0 ? ("%d hours" % h) : nil,
+        m ? ("%d minutes" % m) : ("%d seconds... Orly?")].compact * ' ' 
+    end
+  end
 end
 
 # Main
-songs = filter_by_groups(files,groups)
-print_info(songs)
-
-songs.shuffle.each do |s|
-	`afplay "#{s}"`
-end
+Gramola::Player.new ARGV
+Gramola::Player.print_info
+Gramola::Player.play
